c96521648ce3d2f0318bd14921ee6bad97a62b4b
PintoJR <guimoraespinto@gmail.com>
2026-09-11 19:25:08 -0300

    Solution01

---
+++ app/cinema/bar.py
@@ -0,0 +1,7 @@
+class CinemaBar:
+    @staticmethod
+    def sell_product(product, customer):#(str, Customer())
+        # if not customer:
+        #     print("Invalid customer.")
+        #     return
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-# write your imports here
+from app.cinema.bar import sell_product
+from app.cinema.hall import CinemaHall, movie_session
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     # write you code here
-    pass
+    # Criar instâncias de Customer para cada dict na lista customers
+    customer_instances = []
+    for customer_data in customers:
+        customer = Customer(name=customer_data["name"], food=customer_data["food"])
+        customer_instances.append(customer)
+
+    cleaner_instance = Cleaner(cleaner)
+
+    hall = CinemaHall(hall_number)
+
+    for customer_data in customer_instances:
+            sell_product(customer_data.food, customer_data.name)
+
+    hall.movie_session(movie, customer_instances, cleaner_instance)        
